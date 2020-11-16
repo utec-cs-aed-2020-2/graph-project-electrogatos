26d21df21fd8f73aa0fceb8cd8ddbc8733d5970c
JamesAtachagua <james.atachagua@utec.edu.pe>
2020-11-16 01:59:17 -0300

    feat(parser): Add uGraphMake function
    uGraphMake: function to create a undirected graph for json data
    change int to string Airport ID key in pe.json file

--- src/Parser/parser.h
+++ src/Parser/parser.h
@@ -22,7 +22,7 @@ struct City {
 };
 
 struct Airport {
-    int id;
+    string id;
     string name;
     string city;
     string country;
@@ -34,19 +34,53 @@ class JsonParser {
    protected:
     string jsonPath;
     json data;
-    virtual void insertV() = 0;
-    virtual void insertE() = 0;
+    // virtual void insertV() = 0;
+    // virtual void insertE() = 0;
 
    public:
-    JsonParser(string jsonPath) {
+    JsonParser(string jsonPath) { // readJSON()
         ifstream file(jsonPath);
         data = json::parse(file);
+
+        // Test
+        // for (int i = 0; i < 3; i++) {
+        //     Airport a1;
+        //     a1.id = data[i]["Airport ID"];
+        //     cout << a1.id << endl;
+        // }
     }
 };
 
 class AirportParser : JsonParser {
    public:
     AirportParser(string jsonPath) : JsonParser(jsonPath){};
+
+    void uGraphMake(UnDirectedGraph<string, double> &tempGraph){
+        // Create Vertexes 
+        for (auto& element : data) {
+            Airport a;
+            a.id = element["Airport ID"];
+            int id_int = stoi(a.id);
+            a.name = element["Name"];
+            // a.city = element["City"];
+            // a.country = element["Country"];
+            // // a.latitude = element["Latitude"];
+            // // a.longitude = element["Longitude"];
+            // cout << id_int << " " << a.name << endl;
+            tempGraph.insertVertex(id_int, a.name);
+        }
+        // Create Edges 
+        // Se necesita verificar en la funcion createEdge si existe el edge
+        // y si existe los vertices
+        // for (auto& element : data) { 
+        //     // cout << element["destinations"] << endl;
+        //     string idd = element["Airport ID"];
+        //     for (auto dest : element["destinations"]) {
+        //         string idde = dest;
+        //         tempGraph.createEdge(stoi(idd), stoi(idde), 20);
+        //     }
+        // }
+    };
 };
 
 class CityParser : JsonParser {
