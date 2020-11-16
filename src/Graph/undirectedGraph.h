55314b0ef7918b6fdc7f2f6dccdf3c91ccfebfd2
JamesAtachagua <james.atachagua@utec.edu.pe>
2020-11-16 02:02:55 -0300

    refactor(undirectedGraph): Add new functions
    the functions were placed in order
    add deleteVertex function to delete a vertex
    add deleteEdge function to delete an edge
    add density funciton to calculate the density of a graph
    add isDense function
    add empty function

--- src/Graph/undirectedGraph.h
+++ src/Graph/undirectedGraph.h
@@ -20,7 +20,7 @@ class UnDirectedGraph : public Graph<TV, TE> {
         this->vertexes[id] = v;
 
         return true;
-    };
+    }
 
     bool createEdge(int id1, int id2, TE w) {
         // Se obtienen los vertices de la lista vertexes
@@ -46,6 +46,140 @@ class UnDirectedGraph : public Graph<TV, TE> {
         v2->edges.push_back(e2);
         // v2->edges.sort(compare_nocase);
         return true;
+    }
+
+    bool deleteVertex(int id) {
+        Vertex<TV, TE>* vertex2remove = this->vertexes.at(id);
+        // cout << "Data: " << vertex2remove->data << endl;
+        
+        /* Por cada arista del vertex(id) hay que eliminar la union del otro vertex */
+        Edge<TV, TE>* edge2remove = new Edge<TV, TE>();
+        for (auto edge : vertex2remove->edges) {
+            // cout << "Vertex->: " << edge->vertexes[1]->data << endl;
+            // Se elimina el sentido contrario de la arista
+            for (auto EdgeOfVertex : edge->vertexes[1]->edges){
+                if (EdgeOfVertex->vertexes[1] == vertex2remove){
+                    // cout << "Vertex1 of edge->: " << EdgeOfVertex->vertexes[1]->data << endl;
+                    edge2remove = EdgeOfVertex;
+                }
+            }
+            // se elimina el edge en la lista para kruskal
+            this->graphedges.remove(edge); 
+            this->graphedges.remove(edge2remove); 
+            // se elemina el edge en la propia lista de edges del vertice
+            edge->vertexes[1]->edges.remove(edge2remove);
+        }
+        
+        /* Crear el destructor de Vertex */
+        this->vertexes.erase(id);
+
+        return true; 
+    }
+
+    bool edgee(int start, int end) { 
+        Vertex<TV, TE>* vertex2remove_1 = this->vertexes.at(start);
+        Vertex<TV, TE>* vertex2remove_2 = this->vertexes.at(end);
+        
+        Edge<TV, TE>* edge2remove = new Edge<TV, TE>();
+
+        // delete start -> end
+        for (auto edge : vertex2remove_1->edges){
+            if (edge->vertexes[1] == vertex2remove_2) {
+                edge2remove = edge;
+            }
+        }
+        vertex2remove_1->edges.remove(edge2remove);
+        // se elimina el edge en la lista para kruskal
+        this->graphedges.remove(edge2remove); 
+
+        // delete end -> start
+        edge2remove = nullptr;
+        for (auto edge : vertex2remove_2->edges){
+            if (edge->vertexes[1] == vertex2remove_1) {
+                edge2remove = edge;
+            }
+        }
+        vertex2remove_2->edges.remove(edge2remove);
+
+        return true; 
+    }
+
+    float density() { 
+        if (empty()){
+            // throw "The graph is empty";
+            return 0.0;
+        } else {
+            return ( 2 * this->graphedges.size() ) / ( this->vertexes.size() * ( this->vertexes.size() - 1 ) );
+        }
+    }
+
+    bool isDense(float threshold = 0.5) { 
+        if (density() >= threshold) {
+            return true;
+        } else {
+            return false;
+        }  
+    }
+
+    // saber si el grafo es conexo, se puede usar disjoin set (si existen 2 DS es no conexo)
+    bool isConnected() {
+        // vector<char> data; // Vector para guardar vértices y crear DisjointSet
+        // // Se llena data y el MST Graph
+        // for (auto x : this->vertexes){
+        //     data.push_back(x.second->data);
+        // }
+        // // Se crea un DisjointSet con los vértices del árbol
+        // DisjoinSet<char>* dsc = new DisjoinSet<char>(data);
+        // dsc->MakeSet();
+
+        // // Por cada arista en el árbol se aplica una union si es el caso
+        // for (auto edge_ : this->graphedges){
+        //     int x = dsc->Find(findByVertex(edge_->vertexes[0])); //el mismo vertice
+        //     int y = dsc->Find(findByVertex(edge_->vertexes[1])); //el destino
+
+        //     // Si tienen diferente parent se realiza la union
+        //     if (x != y) {
+        //         // Se agrega el vértice al DisjointSet
+        //         dsc->Union(x, y);
+        //     }
+        // }
+        // // ds->printSets();
+
+        // Si el DisjointSet posee mas de 1 set quiere decir que el grafo es no conexo
+        // Si ya se corrio Kruskal se puede obtener del mismo disjoint set
+        int sets = this->dsgraph->sets();
+
+        return sets <= 1;
+    };
+
+    bool isStronglyConnected() { 
+        throw "This is a undirected graph";
+        return false; 
+    }
+
+    bool empty() { 
+        // El grafo puede tener vertices, pero si no tiene aristas es vacio
+        if (this->graphedges.empty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // TODO
+    void clear() {}
+
+    Vertex<TV, TE>* displayVertex(int id) {
+        Vertex<TV, TE>* v = this->vertexes.at(id);
+        return v;
+    };
+
+    /* Funcion para determinar si existe el vértice en el árbol por id */
+    bool findById(int id) {
+        if (this->vertexes.find(id) == this->vertexes.end())
+            return false;
+        else
+            return true;
     };
 
     /* Funcion para mostrar cada vértice del arbol con sus respectivas aristas */
@@ -61,19 +195,7 @@ class UnDirectedGraph : public Graph<TV, TE> {
         }
     };
 
-    Vertex<TV, TE>* displayVertex(int id) {
-        Vertex<TV, TE>* v = this->vertexes.at(id);
-        return v;
-    };
-
-    /* Funcion para determinar si existe el vértice en el árbol por id */
-    bool findById(int id) {
-        if (this->vertexes.find(id) == this->vertexes.end())
-            return false;
-        else
-            return true;
-    };
-
+    /* Extra functions */
     /* Funcion para determinar si existe el vértice en el árbol por el mismo vértice */
     int findByVertex(Vertex<TV, TE>* v) {
         typename unordered_map<int, Vertex<TV, TE>*>::iterator itr;
@@ -81,14 +203,7 @@ class UnDirectedGraph : public Graph<TV, TE> {
             if (itr->second == v) return itr->first;
         }
         return -1;
-    }
-
-    void clear() {}
-    bool empty() { return true; }
-    bool deleteVertex(int id) { return true; }
-    bool deleteEdge(int id) { return true; }
-    float density() { return 1.0; }
-    bool isDense(float threshold = 0.5) { return true; }
+    }    
 
     /* Función para hallar el Minimun Spanning Tree */
     UnDirectedGraph<TV, TE> execKruskal() {
@@ -108,6 +223,7 @@ class UnDirectedGraph : public Graph<TV, TE> {
 
         // Por cada arista en el árbol se aplica una union si es el caso
         for (auto edges_ : this->graphedges) {
+            // cout << edges_->vertexes[0]->data << " -> " << edges_->vertexes[1]->data << endl;
             Edge<TV, TE>* next_edge = edges_;
 
             int x = ds->Find(findByVertex(next_edge->vertexes[0]));  // el mismo vertice
@@ -126,6 +242,8 @@ class UnDirectedGraph : public Graph<TV, TE> {
         this->dsgraph = ds;
         return BFSGraph;
     };
+
+    /* Prim: https://www.geeksforgeeks.org/prims-algorithm-using-priority_queue-stl/ */
 
     /* Función para realizar la busqueda en profundidad */
     UnDirectedGraph<TV, TE> execDFS() {
@@ -213,37 +331,6 @@ class UnDirectedGraph : public Graph<TV, TE> {
         return BFSGraph;
     };
 
-    // saber si el grafo es conexo, se puede usar disjoin set (si existen 2 DS es no conexo)
-    bool isConnected() {
-        // vector<char> data; // Vector para guardar vértices y crear DisjointSet
-        // // Se llena data y el MST Graph
-        // for (auto x : this->vertexes){
-        //     data.push_back(x.second->data);
-        // }
-        // // Se crea un DisjointSet con los vértices del árbol
-        // DisjoinSet<char>* dsc = new DisjoinSet<char>(data);
-        // dsc->MakeSet();
-
-        // // Por cada arista en el árbol se aplica una union si es el caso
-        // for (auto edge_ : this->graphedges){
-        //     int x = dsc->Find(findByVertex(edge_->vertexes[0])); //el mismo vertice
-        //     int y = dsc->Find(findByVertex(edge_->vertexes[1])); //el destino
-
-        //     // Si tienen diferente parent se realiza la union
-        //     if (x != y) {
-        //         // Se agrega el vértice al DisjointSet
-        //         dsc->Union(x, y);
-        //     }
-        // }
-        // // ds->printSets();
-
-        // Si el DisjointSet posee mas de 1 set quiere decir que el grafo es no conexo
-        // Si ya se corrio Kruskal se puede obtener del mismo disjoint set
-        int sets = this->dsgraph->sets();
-
-        return sets <= 1;
-    };
-    bool isStronglyConnected() { return true; }
 };
 
 #endif
