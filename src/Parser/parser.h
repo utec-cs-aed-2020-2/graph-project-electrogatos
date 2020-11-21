e809aa8e31754d62658891d30c79a9a953975734
JamesAtachagua <james.atachagua@utec.edu.pe>
2020-11-20 22:14:18 -0300

    feat(parser): Adding creation of graph
    Create edges with euclidean distances as weigth
    Verification if a destination of airports doesnt exist

--- src/Parser/parser.h
+++ src/Parser/parser.h
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <sstream>
 #include <stdlib.h>
+#include <string>
 #include <nlohmann/json.hpp>
 #include "json.hpp"
 
@@ -42,7 +43,7 @@ class JsonParser {
         ifstream file(jsonPath);
         data = json::parse(file);
 
-        // Test
+        // For Test
         // for (int i = 0; i < 3; i++) {
         //     Airport a1;
         //     a1.id = data[i]["Airport ID"];
@@ -54,31 +55,47 @@ class JsonParser {
 class AirportParser : JsonParser {
    public:
     AirportParser(string jsonPath) : JsonParser(jsonPath){};
+    unordered_map<int, Airport> airports;
 
     void uGraphMake(UnDirectedGraph<string, double> &tempGraph){
         // Create Vertexes 
         for (auto& element : data) {
             Airport a;
-            a.id = element["Airport ID"];
-            int id_int = stoi(a.id);
+            a.id = element["Airport ID"]; int id_int = stoi(a.id);
             a.name = element["Name"];
-            // a.city = element["City"];
-            // a.country = element["Country"];
-            // // a.latitude = element["Latitude"];
-            // // a.longitude = element["Longitude"];
-            // cout << id_int << " " << a.name << endl;
-            tempGraph.insertVertex(id_int, a.name);
+            a.city = element["City"];
+            a.country = element["Country"];
+            string la = element["Latitude"]; a.latitude = stod(la);
+            string lo = element["Longitude"]; a.longitude = stod(lo);            
+
+            // tempGraph.insertVertex(id_int, a.name);
+            tempGraph.insertVertex(id_int, a.id); // For simplicity
+            this->airports[id_int] = a;
         }
         // Create Edges 
-        // Se necesita verificar en la funcion createEdge si existe el edge
-        // y si existe los vertices
-        // for (auto& element : data) { 
-        //     // cout << element["destinations"] << endl;
-        //     string idd = element["Airport ID"];
-        //     for (auto dest : element["destinations"]) {
-        //         string idde = dest;
-        //         tempGraph.createEdge(stoi(idd), stoi(idde), 20);
-        //     }
+        for (auto& element : data) { 
+            string id_origen = element["Airport ID"];
+            Airport origen = this->airports.at(stoi(id_origen));
+            cout << "Origen: " << id_origen << " " << origen.name << " " << origen.latitude << " " << origen.longitude << endl;
+
+            for (auto dest : element["destinations"]) {
+                string id_dest = dest;
+                double de;
+                try {
+                    Airport destination = this->airports.at(stoi(id_dest));
+                    cout << "  Dest: " << id_dest << " " << destination.name << " " << destination.latitude << " " << destination.longitude << "  ";
+                    de = sqrt( pow( ( destination.latitude - origen.latitude ) , 2.0) + pow( ( destination.longitude - origen.longitude ) , 2.0) );
+                    cout << "Deuclidiana: " << de << endl;
+                    tempGraph.createEdge(stoi(id_origen), stoi(id_dest), de);
+                } catch(const out_of_range &e) {
+                    // cerr << "Exception at " << e.what() << endl;
+                    cout << "  Destination not found!!!" << endl;
+                }
+            }
+        }
+        // For test
+        // for (auto ar : this->airports) {
+        //     cout << ar.first << " " << ar.second.name << " " << ar.second.latitude << " " << ar.second.longitude << endl;
         // }
     };
 };
