fd17b33fd5f831c77a74902898e446585008d725
Estephany Paniagua <estephany.paniagua@utec.edu.pe>
2020-12-16 13:03:59 -0500

    refactor(bfs): changed variables, removed comments

--- src/Graph/Algoritmos/breadthfs.h
+++ src/Graph/Algoritmos/breadthfs.h
@@ -18,7 +18,7 @@ class BFS {
     Graph<TV, TE>* Graph;
     ::Graph<TV, TE>* BFSGraph;
     Vertex<TV, TE>* v_init;
-    list<TV> RecorridoBFS;
+    list<TV> rutaBFS;
 
     BFS(::Graph<TV, TE>& Graph, TV init) {  // tambien sobre grafo dirigido
         this->Graph = &Graph;
@@ -34,7 +34,6 @@ class BFS {
     };
 
     ::Graph<TV, TE>* apply() {
-        // ::Graph<TV, TE> *BFSGraph = new UnDirectedGraph<TV, TE>();
         unordered_map<Vertex<TV, TE>*, bool> visited;
 
         typename unordered_map<int, Vertex<TV, TE>*>::iterator itr;
@@ -45,63 +44,40 @@ class BFS {
             BFSGraph->insertVertex(itr->first, itr->second->data);
         }
 
-        // Para cada vértice en el árbol
-        // for (auto itr_new : this->Graph->vertexes) { // para grafos no conexos
-        // cout << "vi1: " << visited[itr_new.second] << endl;
-        // if (!visited[itr_new.second]) {
-        // cout << "no vi1" << endl;
         // Create a queue of Vertex
         queue<Vertex<TV, TE>*> queue;
-        // Se coloca el vértice actual como visitado y se encola
-        // cout << "Incio: " << this->Graph->vertexes.begin()->second->data << endl;
-        cout << "Incio: " << this->v_init->data << endl;
-        // cout << this->Graph->vertexes.at(1)->data << endl; // for test
-        // visited[this->Graph->vertexes.begin()->second] = true;
-        visited[this->v_init] = true;
-        // visited[this->Graph->vertexes.at(1)] = true; // for test
-        // if (this->Graph->type == 0) {
-        //     visited[itr_new.second] = true;
-        // } else if (this->Graph->type == 1) {
 
-        // } else {
-        //     cout << "Error" << endl;
-        // }
-        // queue.push(this->Graph->vertexes.begin()->second);
+        // Se coloca el vértice actual como visitado y se encola
+        cout << "Incio: " << this->v_init->data << endl;
+        visited[this->v_init] = true;
         queue.push(this->v_init);
-        // queue.push(this->Graph->vertexes.at(1)); // for test
 
         while (!queue.empty()) {
-            // cout << "no empty" << endl;
             Vertex<TV, TE>* v = queue.front();
             cout << " Nodo pop queque: " << v->data << endl;
-            this->RecorridoBFS.push_back(v->data);
+            this->rutaBFS.push_back(v->data);
             queue.pop();
 
             // Para cada vértice adyacente de v
             //--> si no ha sido visitado se marca como visitado y encola
             for (auto edg : v->edges) {
-                // cout << "vi2: " << visited[edg->vertexes[1]] << endl;
                 if (!visited[edg->vertexes[1]]) {
                     cout << "  Nodo push queque: " << edg->vertexes[1]->data << endl;
                     visited[edg->vertexes[1]] = true;
                     queue.push(edg->vertexes[1]);
                     // Se crea una nueva arista en el nuevo Grafo (Peso no importa)
-                    // cout << this->Graph->findByVertex(edg->vertexes[0]) << " " << endl;
                     BFSGraph->createEdge(this->Graph->findByVertex(edg->vertexes[0]),
                                          this->Graph->findByVertex(edg->vertexes[1]), 1);
                 }
             }
         }
-        // }
-        // }
-        // cout << endl;
         return BFSGraph;
     }
 
-    void displayresult(){
+    void displayresult() {
         cout << "Recorrido BFS: " << endl;
-        for (auto e : RecorridoBFS) {
-            cout << " " << e << " ->" ;
+        for (auto e : rutaBFS) {
+            cout << " " << e << " ->";
         }
         cout << endl;
     }
