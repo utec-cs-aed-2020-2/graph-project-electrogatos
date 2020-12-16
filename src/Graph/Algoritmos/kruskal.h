d58c5ff8cd6e76d230e6f942a6d4aa56f95a54c6
Estephany Paniagua <estephany.paniagua@utec.edu.pe>
2020-12-16 13:07:44 -0500

    refactor(kruskal): removed comments

--- src/Graph/Algoritmos/kruskal.h
+++ src/Graph/Algoritmos/kruskal.h
@@ -7,10 +7,10 @@
 #include <unordered_map>
 #include <vector>
 
-#include "../Algoritmos/kruskal.h"
-#include "../undirectedGraph.h"
 #include "../../DisjointSet/disjointSet.h"
+#include "../Algoritmos/kruskal.h"
 #include "../graph.h"
+#include "../undirectedGraph.h"
 
 using namespace std;
 
@@ -19,9 +19,9 @@ class Kruskal {
    public:
     DisjoinSet<TV>* dsgraph;
     UnDirectedGraph<TV, TE> Graph;
-    Kruskal(UnDirectedGraph<TV, TE> &Graph) {
+    Kruskal(UnDirectedGraph<TV, TE>& Graph) {
         this->Graph = Graph;
-        
+
         UnDirectedGraph<TV, TE> BFSGraph = apply();
         Graph.total_sets = this->Graph.total_sets;
     };
@@ -31,12 +31,12 @@ class Kruskal {
     static bool compare_nocase(const Edge<TV, TE>* first, const Edge<TV, TE>* second) {
         return first->weight < second->weight;
     }
-    
+
    public:
-    UnDirectedGraph<TV, TE> apply(){
+    UnDirectedGraph<TV, TE> apply() {
         UnDirectedGraph<TV, TE> BFSGraph;  // MST Graph
-        vector<TV> data;                 // Vector para guardar vértices y crear DisjointSet
-        unordered_map<TV, int> vtx; // Para acomodar los indices
+        vector<TV> data;                   // Vector para guardar vértices y crear DisjointSet
+        unordered_map<TV, int> vtx;        // Para acomodar los indices
 
         // Se llena data y el MST Graph
         int index = 0;
@@ -48,38 +48,28 @@ class Kruskal {
             index++;
         }
 
-        // for (auto e : vtx) {
-        //     cout << "Index: " << e.first << " Data: " << e.second << endl;
-        // }
-
         // Se ordena las aristas de menor a mayor segun su peso
         this->Graph.graphedges.sort(compare_nocase);
+
         // Se crea un DisjointSet con los vértices del árbol
         DisjoinSet<TV>* ds = new DisjoinSet<TV>(data);
         ds->MakeSet();
 
         // Por cada arista en el árbol se aplica una union si es el caso
         for (auto edges_ : this->Graph.graphedges) {
-            // cout << edges_->vertexes[0]->data << " -> " << edges_->vertexes[1]->data << endl;
             Edge<TV, TE>* next_edge = edges_;
             int x = ds->Find(vtx[next_edge->vertexes[0]->data]);  // el mismo vertice
             int y = ds->Find(vtx[next_edge->vertexes[1]->data]);  // el destino
-            // cout << "Index x: " << x << " Index y: " << y << endl;
 
             // Si tienen diferente parent se realiza la union
             if (x != y) {
-                // cout << " Union: (" << x << ", " << y << ")" << endl;
                 ds->Union(x, y);
                 // Se agrega la solución al MST Graph
                 BFSGraph.createEdge(this->Graph.findByVertex(next_edge->vertexes[0]),
-                                    this->Graph.findByVertex(next_edge->vertexes[1]), next_edge->weight);
+                                    this->Graph.findByVertex(next_edge->vertexes[1]),
+                                    next_edge->weight);
             }
-
-            // ds->printSets(); // para mostrar la evolucion de los parents y rank
         }
-        // ds->printSets();
-        // cout << "Total Sets: " << ds->sets() << endl;
-        // cout << endl;
 
         this->Graph.total_sets = ds->sets();
 
